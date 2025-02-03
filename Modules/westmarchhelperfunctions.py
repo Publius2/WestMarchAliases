15a5212ca07cfbff22a9c5a2dd088c0747f00302
Publius2 <mercerj.2015@gmail.com>
2025-02-02 22:46:50 -0500

    finished skill gen, started character init

--- Modules/westmarchhelperfunctions.py
+++ Modules/westmarchhelperfunctions.py
@@ -13,5 +13,22 @@ def parser(args, tags):
     tagged_args = list(map(lambda x: f"{'-' if x in tags else ''}{x}", args))
     return argsparse(tagged_args)
     
-def skillgenerator(name, modifier, advantage="normal"):
-    roll = f"1d20+{modifier}"
+def skillgenerator(name, modifier, advantage="normal", lucky=False, minroll=1):
+    
+    #build dice and modifier for skill
+    roll = '1d20' if advantage == 'normal' else '2d20kh1' if advantage == 'advantage' else '2d20kl1'
+    roll += '' if not lucky else 'r1'
+    roll += '' if minroll == 1 else f'mi{minroll}' 
+    roll += modifier
+
+    return {
+        'skill_name': name,
+        'modifier': modifier,
+        'advantage': advantage,
+        'lucky': lucky,
+        'minroll': f'mi{minroll}',
+        'dice': roll
+    }
+
+def characterDictinit(name):
+    return 
